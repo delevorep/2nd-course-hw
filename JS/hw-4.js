35b20b0ce6adbd4b52982c42742d9378318cd86b
delevorep <leftyknox188@gmail.com>
2026-08-29 17:37:10 +0300

    Корректировка. Задание №5 по JS.

--- JS/hw-4.js
+++ JS/hw-4.js
@@ -1,39 +1,42 @@
 // Task1
 let i = 0;
 while (i < 2) {
-   console.log('Привет'); 
-   i++;
+    console.log('Привет');
+    i++;
 }
 // Task2
 let j = 1;
 while (j <= 5) {
-   console.log(j); 
-   j++;
+    console.log(j);
+    j++;
 }
 // Task3
 let k = 7;
 while (k <= 22) {
-   console.log(k); 
-   k++;
+    console.log(k);
+    k++;
 }
 // Task4
 const obj = {
-  "Коля" : '200',
-  "Вася" : '300',
-  "Петя" : '400',
+    "Коля": '200',
+    "Вася": '300',
+    "Петя": '400',
 }
 for (let key in obj) {
-   console.log(`${key}—${obj[key]}`);
+    console.log(`${key}—${obj[key]}`);
 }
 // Task5
 let n = 1000;
+let m = 1;
 do {
-   n /= 2;
+    n /= 2;
+    m++;
 } while (n >= 50)
 console.log(n);
+console.log(m)
 // Task6
 let friday = 6;
-while (friday <=31) {
-   console.log(`Сегодня пятница, ${friday}-е число. Необходимо подготовить отчет.`);
-   friday += 7;
+while (friday <= 31) {
+    console.log(`Сегодня пятница, ${friday}-е число. Необходимо подготовить отчет.`);
+    friday += 7;
 }
